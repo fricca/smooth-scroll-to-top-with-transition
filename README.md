7c8c6c0c6faa61642d468528c361c0c6adbf1576
fricca <fricca@uk2.net>
2013-04-03 00:08:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,157 @@
+# Smooth Scroll-to-top with CSS transitions
+
+## Description
+
+Source: [http://github.com/fricca/smooth-scroll-to-top-with-transition](http://github.com/fricca/smooth-scroll-to-top-with-transition)
+
+When clicking on the "to top" link the browser smoothly scrolls to the top of the document by using CSS transitions. Older browsers just jump to the top (no, there's no animation fallback).
+
+Test it yourself by scrolling down and clicking on the appearing "to top" link on the right bottom :) For small viewports (below 35em width) the link is always at the page bottom.
+
+### How it's done
+
+When the "to top" link is clicked, a top margin is applied to the html element that smoothly moves the top of the document to the desired position. When the transition finished the new window position is applied and the margin is removed. *Note: I tried it the other way round: first move to window position, then apply transitioned margin, but: slower machines and iOS show visible jump to the new position before starting the transition.*
+
+Flaws: The scrollbar behaviour seems a bit ... odd.
+
+## Test Results
+
+###Working in:
+
+* Desktop
+	* Chrome 25 (Win 7)
+	* Chrome 26 (OSX Lion)
+	* Chrome 27 (Canary) (Win 7)
+	* Firefox 19 (Win 7)
+	* Firefox 9 (Win 7)
+	* Firefox 5 (Win 7)
+	* IE 10 (Win 8 - Metro &amp; Desktop)
+	* Opera 12.12 (Win 7)
+	* Safari 5.1.7 (Win 7)
+	* Safari 6.0.2 (OSX Lion)
+
+
+* Mobile
+	* iOS 4.3 Safari (iPod touch 2)
+	* iOS 5 Safari (iPod touch 4)
+	* iOS 5 Chrome 25 (iPod touch 4)
+	* iOS 6.1 Safari (iPad 2) - little flicker when reaching the top position
+	* iOS 6.1 Chrome (iPad 2)  - little flicker when reaching the top position
+	* Android 2.3 native (Samsung Galaxy S)
+	* Android 2.3 Firefox (Samsung Galaxy S)
+	* Android 2.3 Opera Mobile(Samsung Galaxy S)
+	* Android 2.3 Opera Mobile Beta on (Samsung Galaxy S)
+	* Android 4.0.4 native (Nokia XPeria U)
+	* Android 4.0.4 Firefox 19.0.2 (Nokia XPeria U)
+	* Android 4.0.4 Chrome 25 (Nokia XPeria U)
+	* Android 4.0.4 Chrome 26 beta (Nokia XPeria U)
+	* Android 4.0.4 Opera Mobile 12.10 (Nokia XPeria U)
+
+### Without transitions:
+
+* Desktop
+    * IE 7/8/9
+    * Firefox 2 - 3.6
+
+
+* Mobile
+    * Windows Phone 7.8 (Nokia Lumia 800) uses native smooth scrolling
+
+##Usage
+
+### A. With defaults
+
+#### Basic Example ([Demo](http://demos.corina-rudel.de/sst/sstt/basic))
+
+1. Add the base stylesheet to your document, p.e. put the following in the `head`:
+```html
+	<link rel="stylesheet" href="scrolltotop.css" />
+```
+... or simply copy its contents to your own stylesheet.
+
+2. Add the `scrolltotop.min.js` to your document and initialize the script, p.e. put the following lines before the closing `</body>` tag:
+```html
+	<script src="scrolltotop.min.js"></script>
+	<script>scrollToTop();</script>
+```
+
+With this setup the "link-to-top" will be created by the script and appended at the bottom of your document.
+
+### B. With customized options
+
+Options that can be passed to the `scrollToTop` function as an object (the following code shows the defaults):
+```javascript
+	{
+		// Name of link(s) that trigger(s) scrolling
+		// as selector: #id or .class
+		linkName: '',
+		// distance to scroll (in pixels)
+		// while link is invisible
+		// only used if there's only ONE toplink
+		hiddenDistance: '100',
+		// content of topLink if created by script
+		topLinkContent: 'To Top'
+	}
+```
+
+#### Customized Example 1 ([Demo](http://demos.corina-rudel.de/sst/sstt/custom-1))
+
+Use existing custom link for smooth scrolling, p.e. a link that has <code>id="topscroller"</code> and show the link as soon as the user has scrolled 50px from the top:
+```html
+	<script>
+		scrollToTop({
+			linkName: '#topscroller',
+			hiddenDistance: '50'
+		});
+	</script>
+```
+Note that the option `topLinkContent` will have no effect; the original content of the custom link is always used.
+
+#### Customized Example 2 ([Demo](http://demos.corina-rudel.de/sst/sstt/custom-2))
+
+Use several custom links, p.e. links that have <code>class="toplink"</code>
+```html
+	<script>
+		scrollToTop({
+			linkName: '.toplink',
+		});
+	</script>
+```
+Note that neither the option `hiddenDistance` nor `topLinkContent` will have any effect and the default stylesheet `scrolltotop.css` will only affect the smooth scrolling of the page, not any of the links.
+
+#### Customized Example 3 ([Demo](http://demos.corina-rudel.de/sst/sstt/custom-3))
+
+Let the script create the link but change the default link text and make it always visible:
+```html
+	<script>
+		scrollToTop({
+			hiddenDistance: '0',
+			topLinkContent: 'Scroll to top'
+		});
+	</script>
+```
+
+## Q &amp; A (<small>to myself</small>)
+
+<dl>
+	<dt>Why use margin-top for transition, not transform (that would have better performance)?</dt>
+	<dd>
+		Because fixed elements on the page will move along with the transformed root element, despite being fixed. I couldn't come up with a reliable solution to kind of "revert" the root transformation on the fixed element(s).
+	</dd>
+	<dt>Can this be used for smooth scrolling anywhere, p.e. on one page sites?</dt>
+	<dd>
+		Yes, example soon to come.
+	</dd>
+</dl>
+
+## License
+
+Licensed under the MIT license.
+
+Copyright (c) 2013 [Corina Rudel](http://corina-rudel.de)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ scrolltotop.js
@@ -0,0 +1,396 @@
+/*!
+ * Smooth scroll to top with CSS transitions
+ *
+ * Author: Corina Rudel @friccaW
+ * Version: 1.0
+ * Source: http://github.com/fricca/smooth-scroll-to-top-with-transition
+ */
+
+var scrollToTop = (function(w, d) {
+	var // changable settings
+		settings = {
+			// Name of link(s) that trigger(s) scrolling
+			// as selector: #id or .class
+			linkName: '',
+			// distance to scroll (in pixels) until scroll link is visible
+			// only used if there's only ONE toplink
+			hiddenDistance: '100',
+			// content of topLink if created by script
+			topLinkContent: 'To Top'
+		},
+		// unchangable settings
+		data = {
+			// class to be attached to toplink
+			linkClass: 'sstt',
+			// class for single link
+			singleClass: 'is-single',
+			// class to be set when toplink is hidden
+			hiddenClass: 'is-hidden',
+			// class to be set when toplink is visible
+			visibleClass: 'is-visible',
+			// class to be set on root element to trigger transition
+			animationClass: 'is-animated'
+		},
+		root = d.documentElement,
+		body = d.body,
+		topLink = [],
+		target,
+		distance,
+		startedAnim,
+
+		/**
+		 * HELPER
+		 */
+		MCC = {
+			test: {
+				// Tests for transitionend event
+				transitionEnd: (function transitionEvent() {
+					var el = d.documentElement,
+						transitions = {
+							'transition': 'transitionend',
+							'OTransition':'otransitionend',
+							'MozTransition':'transitionend',
+							'WebkitTransition': 'webkitTransitionEnd'
+						},
+						type,
+						prefix;
+
+					for(type in transitions) {
+						if(typeof transitions[type] !== 'undefined' && typeof el.style[type] !== 'undefined') {
+							return transitions[type];
+						}
+					}
+				}())
+			},
+
+			// Return replacement pattern for certain class (cls)
+			classPattern: function(cls) {
+				var pattern = new RegExp("(^| )" + cls + "( |$)");
+				return pattern;
+			},
+
+			// Check if element (el) has certain class (cls)
+			hasClass: function(el, cls) {
+				var pattern;
+				if(el && cls) {
+					if(typeof el.classList !== 'undefined') {
+						return el.classList.contains(cls);
+					} else {
+						pattern = this.classPattern(cls);
+						return pattern.test(el.className);
+					}
+				}
+			},
+
+			// Get all elements that have certain class
+			getElementsByClass: function(cls) {
+				var foundEls = [],
+					allEls,
+					i,
+					ii,
+					elem;
+
+				if(d.getElementsByClassName) {
+					foundEls = d.getElementsByClassName(cls);
+				} else {
+					if(d.all) {
+						allEls = d.all;
+					} else {
+						allEls = d.getElementsByTagName('*');
+					}
+					for (i = 0, ii = allEls.length; i < ii; i++) {
+						elem = allEls[i];
+						if(MCC.hasClass(elem, cls)) {
+							foundEls[foundEls.length] = elem;
+						}
+					}
+				}
+				return foundEls;
+			},
+
+			// Add class to element
+			addClass: function(el, cls) {
+				var classes;
+
+				if(el && cls) {
+					if(typeof el.classList !== 'undefined') {
+						el.classList.add(cls);
+					} else if(el && cls && !this.hasClass(el, cls)) {
+						if(el.className !== '') {
+							classes = el.className + ' ' + cls;
+						} else {
+							classes = cls;
+						}
+						el.className = classes;
+					}
+				}
+			},
+
+			// Remove class from element
+			removeClass: function(el, cls) {
+				var pattern;
+				if(el && cls) {
+					if(typeof el.classList !== 'undefined') {
+						el.classList.remove(cls);
+					} else {
+						pattern = this.classPattern(cls);
+						// replace matching classname (with potential leading whitespace); $1 is backreference to (^| )
+						el.className = el.className.replace(pattern, "$1");
+						// replace potential trailing whitespace
+						el.className = el.className.replace(/ $/, "");
+					}
+				}
+			},
+
+			// Add eventListener to element
+			// @sources
+			// http://ejohn.org/blog/flexible-javascript-events/
+			// http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html
+			addEvent: function(obj, type, fn) {
+				if(obj && type && fn) {
+					// Add event
+					if(obj.addEventListener) {
+						obj.addEventListener(type, fn, false);
+					} else if(obj.attachEvent) {
+						obj["e" + type + fn] = fn;
+						obj[type + fn] = function() {
+							obj["e" + type + fn](w.event);
+						};
+						obj.attachEvent("on" + type, obj[type + fn]);
+					}
+				}
+			},
+
+			// Nomalize preventDefault
+			stopDefault: function(evt) {
+				if(evt && evt.preventDefault) {
+					evt.preventDefault();
+				} else if(w.event && w.event.returnValue) {
+					w.event.returnValue = false;
+				}
+			}
+		};
+
+	/**
+	 * SPECIFIC FUNCTIONS
+	 */
+
+	function check(config) {
+		var item;
+		for(item in config) {
+			// set defaults to custom settings if valid values are passed in
+			if(config.hasOwnProperty(item) && settings.hasOwnProperty(item)) {
+				settings[item] = config[item];
+			}
+		}
+	}
+
+	function getNameFromSelector(selector) {
+		var name = {};
+		if(selector.indexOf('#') !== -1) {
+			// The Id is everything after the #
+			name.id = selector.substr(selector.indexOf('#') + 1);
+		} else if (selector.indexOf('.') !== -1) {
+			// The class is everything after the .
+			name.className = selector.substr(selector.indexOf('.') + 1);
+		}
+		return name;
+	}
+
+	function clearAnimation() {
+		root.removeEventListener(MCC.test.transitionEnd, endListener, false);
+		MCC.removeClass(root, data.animationClass);
+
+		// Reset margin and move window to top
+		root.style.marginTop = root._marginTop || '';
+		delete(root._marginTop);
+
+		startedAnim = false;
+	}
+
+	function cancelAnimation() {
+		var currentPos = w.pageYOffset || root.scrollTop,
+			marginTop = parseInt(w.getComputedStyle(root, null).getPropertyValue("margin-top"), 10) || 0,
+			newPos;
+
+		if(startedAnim) {
+			newPos = currentPos - marginTop;
+			clearAnimation();
+			w.scrollTo(0, newPos);
+		}
+	}
+
+	function endListener(event) {
+		if(event.propertyName === 'margin-top') {
+			clearAnimation();
+			w.scrollTo(0, 0);
+		}
+	}
+
+	function scrollIt() {
+		var rootTop;
+
+		// clean up if something went wrong before
+		if(startedAnim) {
+			clearAnimation();
+		}
+
+		distance = w.pageYOffset || root.scrollTop;
+
+		if(distance) {
+			// smooth scrolling if browser supports transitionend event
+			if(MCC.test.transitionEnd) {
+				// check if root element has own top margin
+				if(w.getComputedStyle) {
+					rootTop = parseInt(w.getComputedStyle(root, null).getPropertyValue("margin-top"), 10);
+					if(rootTop) {
+						distance = distance + rootTop;
+					}
+				}
+
+				// Store style.marginTop for later re-set
+				root._marginTop = root.style.marginTop;
+
+				// add eventListener for transitionend
+				root.addEventListener(MCC.test.transitionEnd, endListener, false);
+
+				// add is-animated class to root element
+				MCC.addClass(root, data.animationClass);
+
+				root.style.marginTop = distance + 'px';
+				// Set indicator for started transition
+				// used to clean up if something went wrong
+				startedAnim = true;
+			} else {
+				// Move window to top
+				w.scrollTo(0, 0);
+			}
+		}
+	}
+
+	function listener(event) {
+		var evTarget = event.target || event.srcElement; // normal || IE <= 8;
+		MCC.stopDefault(event);
+
+		scrollIt();
+	}
+
+	function checkScroll(link) {
+		var scrollTimer;
+
+		// check if there's an unfinished transition left
+		// might happen if the transionend event didn't fire
+		// p.e. if the transition is set for the wrong properties or
+		// left out completely
+		// OR if the transition is disturbed by manual scrolling
+		if(startedAnim) {
+			cancelAnimation();
+		}
+
+		// Check if toplink should be visible or hidden
+		if(link && settings.hiddenDistance) {
+			// check for scroll position every 100ms
+			w.clearTimeout(scrollTimer);
+			scrollTimer = setTimeout(function() {
+				var top = w.pageYOffset || root.scrollTop;
+				if(top >= settings.hiddenDistance) {
+					MCC.removeClass(link, data.hiddenClass);
+					MCC.addClass(link, data.visibleClass);
+				} else {
+					MCC.addClass(link, data.hiddenClass);
+					MCC.removeClass(link, data.visibleClass);
+				}
+			}, 100);
+		}
+	}
+
+	function createLink() {
+		var link;
+
+		link = d.createElement('a');
+		link.href="#";
+
+		if(typeof link.textContent !== 'undefined') {
+			link.textContent = settings.topLinkContent;
+		} else {
+			link.innerText = settings.topLinkContent;
+		}
+		body.appendChild(link);
+
+		return link;
+	}
+
+	function prepareLinks(links) {
+		var i,
+			ii,
+			oneLink;
+
+		// for all links
+		for (i = 0, ii = links.length; i < ii; i++) {
+			MCC.addClass(links[i], data.linkClass);
+			MCC.addEvent(links[i], 'click', listener);
+		}
+
+		// If only one link and hiddenDistance is set
+		if(links.length === 1) {
+			MCC.addClass(links[0], data.singleClass);
+			if(settings.hiddenDistance > 0) {
+				oneLink = links[0];
+				MCC.addClass(links[0], data.hiddenClass);
+			} else {
+				MCC.addClass(links[0], data.visibleClass);
+			}
+		}
+
+		// Listener for scrolling
+		// used to show/hide toplink (oneLink) on hiddenDistance
+		// and to cancel Animation if something went wrong
+		// ony needed if either there's only one top link or
+		// if transitions are supported
+		if(oneLink || MCC.test.transitionEnd) {
+			MCC.addEvent(w, 'scroll',
+				function() {
+					checkScroll(oneLink);
+				}
+			);
+		}
+	}
+
+	function init(config) {
+		var topLinkName;
+
+		// Check for user defined configuration
+		if(config) {
+			check(config);
+		}
+
+		// Check for custom link selector
+		if(settings.linkName) {
+			// Get link name, returns either .id or .className
+			topLinkName = getNameFromSelector(settings.linkName);
+
+			if(topLinkName.hasOwnProperty('className')) {
+				// Try to get link(s) from className
+				topLink = MCC.getElementsByClass(topLinkName.className);
+			} else if(topLinkName.hasOwnProperty('id')) {
+				// Try to get link from ID
+				topLink[0] = d.getElementById(topLinkName.id);
+			}
+
+			// If neither a useful class or ID are given: the end
+			if(!topLink[0]) {
+				return;
+			}
+		} else {
+			// If no custom selector set: create link
+			topLink[0] = createLink();
+		}
+
+		// Prepare link
+		if(topLink.length > 0) {
+			prepareLinks(topLink);
+		}
+	}
+
+	return init;
+}(window, document));
